a76b4dc6e09d4f21d50e5f52872635f39f347ebb
Gabriel Cesario <cesario203@outlook.com>
2021-04-04 14:40:56 -0300

    Finalizando testes de ping, conexão de desconexão

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
-var io = require('socket.io')(4001,{
-    path:'/',
+var io = require('socket.io')(4001, {
+    path: '/',
     cors: {
         origin: "*",
         methods: ["GET", "POST"]
-      }
+    }
 })
-const {EventConstants} = require('./src/config/constants.js')
+const { EventConstants } = require('./src/config/constants.js')
 
 
 io.httpServer.on(EventConstants.LISTENING, function () {
-  console.log('listening on port', io.httpServer.address().port)
+    console.log('listening on port', io.httpServer.address().port)
 
-  io.on(EventConstants.CONNECTION,socket=>
-  {
-    socket.on(EventConstants.DISCONNECT, () => {
-        io.emit(EventConstants.NO_OF_CONNECTIONS, Object.keys(io.sockets.connected).length)
-    })
+    io.on(EventConstants.CONNECTION, socket => {
+        console.log('id do socket ==> ', socket.id)
+        socket.on(EventConstants.CHAT_MESSAGE, (msg) => {
+            socket.broadcast.emit(EventConstants.CHAT_MESSAGE, msg)
+        })
 
-    socket.on(EventConstants.CHAT_MESSAGE, (msg) => {
-        socket.broadcast.emit(EventConstants.CHAT_MESSAGE, msg)
-    })
-    socket.on(EventConstants.JOIN, (name) => {
-        socket.broadcast.emit(EventConstants.JOIN, name)
-    })
-    socket.on(EventConstants.LEAVE, (name) => {
-        socket.broadcast.emit(EventConstants.LEAVE, name)
-    })
+        socket.on(EventConstants.DISCONNECT, teste =>
+            {
+                console.log('kk sai')
+            })
+        
+        socket.on(EventConstants.JOIN, (name) => {
+            socket.broadcast.emit(EventConstants.JOIN, name)
+        })
+        socket.on(EventConstants.LEAVE, (name) => {
+            socket.broadcast.emit(EventConstants.LEAVE, name)
+        })
 
-    socket.on(EventConstants.TYPING, (data) => {
-        socket.broadcast.emit(EventConstants.TYPING, data)
-    })
-    socket.on(EventConstants.STOP_TYPING, () => {
-        socket.broadcast.emit(EventConstants.STOP_TYPING)
-    })
+        socket.on(EventConstants.TYPING, (data) => {
+            socket.broadcast.emit(EventConstants.TYPING, data)
+        })
+        socket.on(EventConstants.STOP_TYPING, () => {
+            socket.broadcast.emit(EventConstants.STOP_TYPING)
+        })
 
-    socket.on(EventConstants.PING_SERVER, () => {
-        console.log('chegou aqui')
+        socket.on(EventConstants.PING_SERVER, (data) => {
+            console.log('ping ',data)
+        })
     })
-  })
 })
